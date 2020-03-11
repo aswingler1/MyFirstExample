a435866ce1e21c685426b49d491cfab1f719aa3d
aswingler1 <36062866+aswingler1@users.noreply.github.com>
2020-03-11 11:53:56 -0700

    Add files via upload

---
+++ MyFirstExample/README.md
@@ -0,0 +1,1 @@
+https://travis-ci.org/aswingler1/MyFirstExample.svg?branch=master
---
+++ MyFirstExample/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+	int x; //re-add int y
+    cout<<"Hi, please enter two numbers: ";
+    cin>>x>>y;
+
+    return 0;
+}
